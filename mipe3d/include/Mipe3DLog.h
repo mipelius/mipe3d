0d07dea11ac47fd119c4d7dfa533b12220ce089f
Miika Pelkonen <pelkonen.miika@gmail.com>
2019-04-04 06:49:48 +0300

    Add vec3 and mat4x4 logging support

--- mipe3d/include/Mipe3DLog.h
+++ mipe3d/include/Mipe3DLog.h
@@ -1,13 +1,22 @@
 #pragma once
 
+#include "apidef.h"
+
 #include <iostream>
 #include <string>
 #include <vec3.hpp>
+#include <mat4x4.hpp>
 
 // NOTE: for verbose logging (showing file, line number)
 // #define MIPE3D_LOG_VERBOSE
 
 // ---- internal helpers, not meant to be used by the client code ----
+
+MIPE3D_API std::ostream& operator << (
+    std::ostream& out, const glm::vec3& vec3);
+
+MIPE3D_API std::ostream& operator << (
+    std::ostream& out, const glm::mat4x4& mat4x4);
 
 #ifdef MIPE3D_LOG_VERBOSE
 #define _MIPE3D_LOG_INTERNAL(outputStream, preMessage, message) \
---
+++ mipe3d/src/Mipe3DLog.cpp
@@ -0,0 +1,49 @@
+#include "Mipe3DLog.h"
+#include <iomanip>
+
+std::ostream& operator << (std::ostream& out, const glm::vec3& vec)
+{
+    out << "(" << vec.x << ", " << vec.y << ", " << vec.z << ")";
+    return out;
+}
+
+std::ostream& operator << (std::ostream& out, const glm::mat4x4& mat4x4)
+{
+    using namespace std;
+
+    // store current format
+    ios state(nullptr);
+    state.copyfmt(out);
+    
+    const glm::mat4x4& m = mat4x4;
+
+    // set new format
+    out << scientific;
+
+    // print the matrix into the stream
+    out << " ["  << m[0][0]
+        << " "  << m[1][0]
+        << " "  << m[2][0]
+        << " "  << m[3][0]
+        << "\n"
+        << "  "  << m[0][1]
+        << " "  << m[1][1]
+        << " "  << m[2][1]
+        << " "  << m[3][1]
+        << "\n"
+        << "  "  << m[0][2]
+        << " "  << m[1][2]
+        << " "  << m[2][2]
+        << " "  << m[3][2]
+        << "\n"
+        << "  "  << m[0][3]
+        << " "  << m[1][3]
+        << " "  << m[2][3]
+        << " "  << m[3][3] 
+        << "]";
+    
+    // restore the original format
+    out.copyfmt(state);
+
+    return out;
+}
