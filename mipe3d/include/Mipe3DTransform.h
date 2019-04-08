f9375ccd253eb080ac29dd33180406e9db064850
Miika Pelkonen <pelkonen.miika@gmail.com>
2019-04-08 08:15:00 +0300

    Implement Transform methods

--- mipe3d/include/Mipe3DTransform.h
+++ mipe3d/include/Mipe3DTransform.h
@@ -1,8 +1,10 @@
 #pragma once
 
+#include "apidef.h"
+
 #include <vec3.hpp>
 #include <mat4x4.hpp>
-#include <list>
+#include <gtc/quaternion.hpp>
 
 namespace mipe3d
 {
@@ -12,21 +14,21 @@ class Transform
 public:
     Transform();
 
-    glm::mat4x4 getMatrix() const;
+    MIPE3D_API glm::mat4x4 getMatrix() const;
 
-    const glm::vec3 &getPosition() const;
-    void setPosition(const glm::vec3 &position);
+    MIPE3D_API const glm::vec3 &getPosition() const;
+    MIPE3D_API void setPosition(const glm::vec3 &position);
 
-    const glm::vec3 &getEulerAngles() const;
-    void setEulerAngles(const glm::vec3 &rotation);
+    MIPE3D_API glm::vec3 getEulerAngles() const;
+    MIPE3D_API void setEulerAngles(const glm::vec3 &rotation);
 
-    const glm::vec3 &getScale() const;
-    void setScale(const glm::vec3 &scale);
+    MIPE3D_API const glm::vec3 &getScale() const;
+    MIPE3D_API void setScale(const glm::vec3 &scale);
 
 private:
-    glm::vec3 m_position;
-    glm::vec3 m_rotation;
-    glm::vec3 m_scale;
+    glm::vec3 m_position = glm::vec3(0.0f, 0.0f, 0.0f);
+    glm::quat m_rotation = glm::quat();
+    glm::vec3 m_scale = glm::vec3(1.0f, 1.0f, 1.0f);
 };
 
 } // namespace mipe3d
--- mipe3d/src/Mipe3DTransform.cpp
+++ mipe3d/src/Mipe3DTransform.cpp
@@ -1,5 +1,7 @@
 #include "Mipe3DTransform.h"
 
+#include <gtx/transform.hpp>
+
 namespace mipe3d
 {
 
@@ -9,7 +11,11 @@ Transform::Transform()
 
 glm::mat4x4 Transform::getMatrix() const
 {
-    return glm::mat4x4(1);
+    auto scaleMatrix = glm::scale(m_scale);
+    auto rotationMatrix = glm::mat4_cast(m_rotation);
+    auto translationMatrix = glm::translate(m_position);
+
+    return translationMatrix * rotationMatrix * scaleMatrix;
 }
 
 const glm::vec3 &Transform::getPosition() const
@@ -22,14 +28,14 @@ void Transform::setPosition(const glm::vec3 &position)
     m_position = position;
 }
 
-const glm::vec3 &Transform::getEulerAngles() const
+glm::vec3 Transform::getEulerAngles() const
 {
-    return m_rotation;
+    return glm::eulerAngles(m_rotation);
 }
 
 void Transform::setEulerAngles(const glm::vec3 &rotation)
 {
-    m_rotation = rotation;
+    m_rotation = glm::quat(rotation);
 }
 
 const glm::vec3 &Transform::getScale() const
