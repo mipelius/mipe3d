4bb3394299d195622e8b2d83c95cb5981773d738
Miika Pelkonen <pelkonen.miika@gmail.com>
2019-04-12 06:44:08 +0300

    Implement Keyboard::isKeyDown(...)

--- mipe3d/src/Mipe3DInput.cpp
+++ mipe3d/src/Mipe3DInput.cpp
@@ -12,6 +12,44 @@ namespace mipe3d
 
 // ------------------------ keyboard ------------------------
 
+static Key convertSDLScancodeToKey(SDL_Scancode scancode)
+{
+    switch (scancode)
+    {
+    case SDL_SCANCODE_UP: return Key::UP;
+    case SDL_SCANCODE_DOWN: return Key::DOWN;
+    case SDL_SCANCODE_LEFT: return Key::LEFT;
+    case SDL_SCANCODE_RIGHT: return Key::RIGHT;
+    case SDL_SCANCODE_W: return Key::W;
+    case SDL_SCANCODE_S: return Key::S;
+    case SDL_SCANCODE_A: return Key::A;
+    case SDL_SCANCODE_D: return Key::D;
+    case SDL_SCANCODE_SPACE: return Key::SPACE;
+    case SDL_SCANCODE_ESCAPE: return Key::ESC;
+    default:
+        return Key::UNKNOWN;
+    }
+}
+
+static SDL_Keycode convertKeyToSDLScancode(Key key)
+{
+    switch (key)
+    {
+    case Key::UP: return SDL_SCANCODE_UP;
+    case Key::DOWN: return SDL_SCANCODE_DOWN;
+    case Key::LEFT: return SDL_SCANCODE_LEFT;
+    case Key::RIGHT: return SDL_SCANCODE_RIGHT;
+    case Key::W: return SDL_SCANCODE_W;
+    case Key::S: return SDL_SCANCODE_S;
+    case Key::A: return SDL_SCANCODE_A;
+    case Key::D: return SDL_SCANCODE_D;
+    case Key::SPACE: return SDL_SCANCODE_SPACE;
+    case Key::ESC: return SDL_SCANCODE_ESCAPE;
+    default:
+        return SDL_SCANCODE_UNKNOWN;
+    }
+}
+
 Keyboard::Keyboard()
 {
 }
@@ -21,16 +59,29 @@ Keyboard::~Keyboard()
 
 bool Keyboard::isKeyDown(Key key) const
 {
-    return false;
+    auto scancode = convertKeyToSDLScancode(key);
+
+    const Uint8* state = SDL_GetKeyboardState(NULL);
+
+    if (scancode == SDL_SCANCODE_UNKNOWN)
+    {
+        return false;
+    }
+    else
+    {
+        return state[scancode];
+    }
 }
 
 bool Keyboard::keyPressed(Key key) const
 {
+    assert(false && "Keyboard::keyPressed(...) not implemented");
     return false;
 }
 
 bool Keyboard::keyReleased(Key key) const
 {
+    assert(false && "Keyboard::keyReleased(...) not implemented");
     return false;
 }
 
